3644c06768a0674183ab0a9c65fb63cc885faf9b
johnmccartin <john.mccartin@gmail.com>
2017-04-06 17:59:03 -0400

    troubleshoot deploy

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const io = require('socket.io')(server);
  * Connect to MongoDB.
  */
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGODB_URI );
 mongoose.connection.on('error', () => {
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
   process.exit();
@@ -82,7 +82,7 @@ app.use(session({
   saveUninitialized: true,
   secret: process.env.SESSION_SECRET,
   store: new MongoStore({
-    url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
+    url: process.env.MONGOLAB_URI || process.env.MONGODB_URI,
     autoReconnect: true
   })
 }));
